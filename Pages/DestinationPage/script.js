e6527ac6f40aaabee87c7a411c8cbc8d7f0d68ac
sonlam <19522133@gm.uit.edu.vn>
2021-12-13 14:23:02 +0700

    fix destination

---
+++ Pages/DestinationPage/script.js
@@ -0,0 +1,28 @@
+$(".destination-row").owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 2000,
+    smartSpeed: 1000,
+    nav: true,
+    navText: ['<ion-icon class="nav-icon prev-icon" name="chevron-back-outline"></ion-icon>','<ion-icon class="nav-icon next-icon" name="chevron-forward-outline"></ion-icon>']
+});
+$(".destination-row-right").owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 2000,
+    smartSpeed: 1000,
+    nav: true,
+    navText: ['<ion-icon class="nav-icon prev-icon" name="chevron-back-outline"></ion-icon>','<ion-icon class="nav-icon next-icon" name="chevron-forward-outline"></ion-icon>']
+});
+
+$(".feature-slider").owlCarousel({
+    loop: true,
+    autoplay: false,
+    margin: 20,
+    width: 300,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    smartSpeed: 2500,
+    nav: true,
+    navText: ['<ion-icon class="nav-icon prev-icon" name="chevron-back-outline"></ion-icon>','<ion-icon class="nav-icon next-icon" name="chevron-forward-outline"></ion-icon>']
+});
