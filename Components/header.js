c0fd91c866c20fffad7a354c6b781570b72f8c30
sonlam <19522133@gm.uit.edu.vn>
2021-12-15 21:12:59 +0700

    register-page

--- Components/header.js
+++ Components/header.js
@@ -1,7 +1,7 @@
 (function () {
-    const header = document.querySelector('.header');
+  const header = document.querySelector('.header');
 
-    header.innerHTML = `<div class="header-top">
+  header.innerHTML = `<div class="header-top">
 <div id="preloader">
     <img src="../../Public/Images/preloader.png" alt="preloader" class="preloader-icon">
 </div>
@@ -53,7 +53,7 @@
     </div>
     <div class="dropdown-box-item">
         <ion-icon name="person-circle" class="dropdown-box-icon"></ion-icon>
-        <p>My Account</p>
+        <a href="../LoginPage/index.html">Sign in</a>
     </div>
     <div class="dropdown-box-item">
         <ion-icon name="settings" class="dropdown-box-icon"></ion-icon>
@@ -68,64 +68,64 @@
 <div class="header-bottom">
 <div class="title-panel">
     <h2 class="title">${
-        header.classList.contains('tour')
-            ? 'Tour Package'
-            : header.classList.contains('about')
-            ? 'About Us'
-            : header.classList.contains('destination')
-            ? 'Destinations'
-            : header.classList.contains('blog')
-            ? 'Blogs'
-            : header.classList.contains('page')
-            ? 'Pages'
-            : header.classList.contains('contact')
-            ? 'Contact Us'
-            : 'Others'
+      header.classList.contains('tour')
+        ? 'Tour Package'
+        : header.classList.contains('about')
+        ? 'About Us'
+        : header.classList.contains('destination')
+        ? 'Destinations'
+        : header.classList.contains('blog')
+        ? 'Blogs'
+        : header.classList.contains('page')
+        ? 'Pages'
+        : header.classList.contains('contact')
+        ? 'Contact Us'
+        : 'Others'
     }</h2>
     <h4 class="subtitle">Home <span>> ${
-        header.classList.contains('tour')
-            ? 'Tour Package'
-            : header.classList.contains('about')
-            ? 'About Us'
-            : header.classList.contains('destination')
-            ? 'Destinations'
-            : header.classList.contains('blog')
-            ? 'Blogs'
-            : header.classList.contains('page')
-            ? 'Pages'
-            : header.classList.contains('contact')
-            ? 'Contact Us'
-            : 'Others'
+      header.classList.contains('tour')
+        ? 'Tour Package'
+        : header.classList.contains('about')
+        ? 'About Us'
+        : header.classList.contains('destination')
+        ? 'Destinations'
+        : header.classList.contains('blog')
+        ? 'Blogs'
+        : header.classList.contains('page')
+        ? 'Pages'
+        : header.classList.contains('contact')
+        ? 'Contact Us'
+        : 'Others'
     }</span></h4>
 </div>
 <div class="overlay"></div>
 </div>`;
 
-    const dropdownBox = document.querySelector('.dropdown-box');
-    const personIcon = document.querySelector('#personIcon');
-    document.onclick = (e) => {
-        if (e.target.id === 'personIcon') {
-            dropdownBox.classList.toggle('show');
-        } else {
-            dropdownBox.classList.remove('show');
-        }
-    };
-    const headerMiddle = header.querySelector('.header-middle');
-    window.onscroll = () => {
-        if (window.scrollY !== 0) {
-            headerMiddle.classList.add('sticky');
-        } else {
-            headerMiddle.classList.remove('sticky');
-        }
-    };
+  const dropdownBox = document.querySelector('.dropdown-box');
+  const personIcon = document.querySelector('#personIcon');
+  document.onclick = (e) => {
+    if (e.target.id === 'personIcon') {
+      dropdownBox.classList.toggle('show');
+    } else {
+      dropdownBox.classList.remove('show');
+    }
+  };
+  const headerMiddle = header.querySelector('.header-middle');
+  window.onscroll = () => {
+    if (window.scrollY !== 0) {
+      headerMiddle.classList.add('sticky');
+    } else {
+      headerMiddle.classList.remove('sticky');
+    }
+  };
 })();
 
 (function () {
-    const preloader = document.querySelector('#preloader');
-    setTimeout(() => {
-        preloader.style.opacity = '0';
-    }, 2000);
-    setTimeout(() => {
-        preloader.style.display = 'none';
-    }, 2500);
+  const preloader = document.querySelector('#preloader');
+  setTimeout(() => {
+    preloader.style.opacity = '0';
+  }, 2000);
+  setTimeout(() => {
+    preloader.style.display = 'none';
+  }, 2500);
 })();
---
+++ Pages/RegisterPage/script.js
@@ -0,0 +1,19 @@
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const listInput = document.querySelectorAll('.login-form .right-panel .input input');
+const signInButton = document.querySelector('#SignIn-button');
+
+signInButton.onclick = (e) => {
+    e.preventDefault();
+    if (emailInput.value !== 'abc@gmail.com') {
+        const errorMessage = document.querySelector('#email + .error-message');
+        errorMessage.textContent = 'Email không chính xác';
+        emailInput.focus();
+    } else if (passwordInput.value !== 'abcdef123456') {
+        const errorMessage = document.querySelector('#password + .error-message');
+        errorMessage.textContent = 'Mật khẩu không chính xác';
+        passwordInput.focus();
+    } else {
+        window.location = 'http://127.0.0.1:5500/Pages/HomePage/index.html';
+    }
+};
